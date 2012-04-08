011366bfd034a90a32c70772f26c3b7ccd619529
Shane Clark <ssclark@cs.umass.edu>
2012-04-07 20:39:31 -0400

    Added a hook to cancel all requests containg Set-Cookie headers.

--- background.js
+++ background.js
@@ -8,3 +8,16 @@ chrome.cookies.onChanged.addListener(
     }
 );
 
+// Hooks all web requests after headers are received and denies those setting
+// cookies. 
+chrome.webRequest.onHeadersReceived.addListener(
+  function(details) {
+		for(var i in details.responseHeaders) {
+			if(details.responseHeaders[i].name == 'Set-Cookie') {
+				console.log("Ate a cookie!");
+				return {cancel: true};
+			}
+		}
+  },
+  {urls: ["<all_urls>"]},
+	["blocking", "responseHeaders"]);
