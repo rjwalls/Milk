4690b534ee553677bed759b7dba59c3aef9ab8a6
Robert Walls <rjwalls@cs.umass.edu>
2012-04-09 13:41:59 -0400

    Added the onBeforeSendHeaders listener to intercept and inject cookie headers

--- background.js
+++ background.js
@@ -16,12 +16,14 @@ chrome.webRequest.onHeadersReceived.addListener(
 //				return {cancel: true};
 			}
 		}
+		
+	
   },
   {urls: ["<all_urls>"]},
 	["blocking", "responseHeaders"]);
 
-// Logs all request headers containing Cookies.
-chrome.webRequest.onBeforeSendHeaders.addListener(
+// Logs all request headers containing Cookies. This function will record any header modification we make in our "onBeforeSendHeaders" listener.
+chrome.webRequest.onSendHeaders.addListener(
 	function(details) {
 		for(var i in details.requestHeaders) {
 			if(details.requestHeaders[i].name == 'Cookie') {
@@ -30,4 +32,42 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 		}
 	},
 	{urls: ["<all_urls>"]},
-	["blocking", "requestHeaders"]);
+	["requestHeaders"]);
+	
+	
+// Logs all request headers containing Cookies.
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    function(details) {
+    
+        //chrome shouldn't be able to find any cookies for this domain, but we should check and make sure
+        for(var i in details.requestHeaders) {
+            if(details.requestHeaders[i].name === 'Cookie') {
+                //Oh noes, cookie found, better remove it.
+                console.log(details.requestHeaders[i]);
+                details.requestHeaders.splice(i, 1);
+            }
+        }
+        
+      
+        // We have to make a call to our store and pull all cookies. Then we add those those cookies to the request via headers. Header name should be 'Cookie' and value should be 'key=value'
+        var headerString = getCookieString(details.tabId, details.url);
+        
+        var cookieHeader = {name:"Cookie", value:headerString};
+        
+        details.requestHeaders.push(cookieHeader);
+    
+        var responseObj = { requestHeaders:details.requestHeaders };
+        
+        return responseObj;
+    },
+    {urls: ["<all_urls>"]},
+    ["blocking", "requestHeaders"]);
+
+// This function gets all cookies associated with a certain tab and domain and returns objects of type {"value", "key"}    
+function getCookieString(tabId, url){
+    //TODO: Need to implement this.
+    
+    return "test_key=test_value; test1_key=test1_value";
+
+}
+
