6e2c9bf5c547ad46ea1434aa972562be9b564955
Robert Walls <rjwalls@cs.umass.edu>
2012-04-17 16:04:49 -0400

    Added code for firing custom events from within the cookie setter.

--- contentscript.js
+++ contentscript.js
@@ -14,11 +14,28 @@ function detectLogin() {
     }
 }
 
-var actualCode = 'var _cookie = document.cookie; document.__defineSetter__("cookie", function(the_cookie) {_cookie = the_cookie;} ); document.__defineGetter__("cookie", function() {alert(_cookie);} );'
+
+
+var actualCode = 
+    'var _newDiv = document.createElement("div");' +
+    '_newDiv.setAttribute("id","cookieDiv");' +
+    'document.documentElement.appendChild(_newDiv);' +
+    'var customEvent = document.createEvent("Event");' +
+    'customEvent.initEvent("myCustomEvent", true, true);' +
+    '_newDiv.innerText = "Nom Nom Nom";' +
+    'var _cookie = document.cookie;' + 
+    'console.log(document.cookie);' + 
+    'document.__defineSetter__("cookie", function(the_cookie) {_cookie = the_cookie; console.log("Our cookie setter was called! " + the_cookie); _newDiv.innerText = the_cookie; _newDiv.dispatchEvent(customEvent);} ); document.__defineGetter__("cookie", function() {console.log("Our getter was called"); _newDiv.dispatchEvent(customEvent);} );'
 
 var script = document.createElement('script');
 script.appendChild(document.createTextNode(actualCode));
 (document.head || document.documentElement).appendChild(script);
 script.parentNode.removeChild(script);
 
+console.log(document.cookie);
+
 detectLogin();
+
+document.getElementById("cookieDiv").addEventListener('myCustomEvent', function() { console.log("Event Fired! " + document.getElementById("cookieDiv").innerText); });
+
+
